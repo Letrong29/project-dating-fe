b172142b5a38b896681ae498449506c6f0cb508b
unknown <you@example.com>
2022-10-18 11:40:43 +0700

    reset-password

--- dating-app-fe/src/app/account/account.module.ts
+++ dating-app-fe/src/app/account/account.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountRoutingModule } from './account-routing.module';
+import { ResetPasswordComponent } from './component/reset-password/reset-password.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [ResetPasswordComponent],
+  exports: [
+    ResetPasswordComponent
+  ],
   imports: [
     CommonModule,
     AccountRoutingModule
