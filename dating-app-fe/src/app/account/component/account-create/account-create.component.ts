9875f3831efb7211408aa78e519d1596494eb997
letrong29 <letrong2929Gmail.com>
2022-10-19 09:18:03 +0700

    commit create account

---
+++ dating-app-fe/src/app/account/component/account-create/account-create.component.ts
@@ -0,0 +1,60 @@
+import { Component, OnInit } from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import {RegisterAccountService} from '../../service/register-account.service';
+import {Account} from '../../model/account';
+
+@Component({
+  selector: 'app-account-create',
+  templateUrl: './account-create.component.html',
+  styleUrls: ['./account-create.component.css']
+})
+export class AccountCreateComponent implements OnInit {
+
+  registerAccount: FormGroup;
+
+  account: Account;
+
+  errorList: any;
+
+  constructor(private registerAccountService: RegisterAccountService) {
+    this.registerAccount = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.pattern(/^[\w-\.]+@(gmail\.)+(com)$/)]),
+
+      phone: new FormControl('', [Validators.required, Validators.pattern(/^(090|093|097)\d{7}$/)]),
+
+      passwordGroup: new FormGroup({
+        password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+
+        passwordConfirm: new FormControl()
+      }, [this.checkPassword]),
+    })
+  }
+
+  ngOnInit(): void {
+  }
+
+  save() {
+    if (this.registerAccount.valid){
+      let account: Account = this.registerAccount.value
+      account.password = this.registerAccount.controls.passwordGroup.get('password').value;
+      console.log(account);
+      this.registerAccountService.save(account).subscribe(next => {
+      },error => {
+        this.errorList = error.error
+      })
+    }
+  }
+
+  private checkPassword(passwordGroup: AbstractControl) {
+
+    let password = passwordGroup.get('password').value;
+
+    let passwordConfirm = passwordGroup.get('passwordConfirm').value;
+
+    if (password !== passwordConfirm) {
+      return {'checkPasswordConfirm': true};
+    }
+    return null;
+  }
+
+}
