31813552c425862dfe7e5019b21879cc8822d1b7
thaildh95 <thaildh.rtbed@gmail.com>
2022-10-20 13:28:32 +0700

    thai up date

--- dating-app-fe/src/app/user/component/post/post.component.ts
+++ dating-app-fe/src/app/user/component/post/post.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {UserService} from "../../service/user.service";
 import {Post} from "../../model/post";
 import {NewFeed} from "../../model/new-feed";
+import {SuggestService} from "../../../service/request/suggest.service";
+import {User} from "../../model/user";
 
 @Component({
   selector: 'app-post',
@@ -9,16 +11,28 @@ import {NewFeed} from "../../model/new-feed";
   styleUrls: ['./post.component.css']
 })
 export class PostComponent implements OnInit {
+  suggestList : User[];
   listShow : NewFeed[];
-   id = 1;
-  constructor(private service: UserService) {
+  size=4;
+  myId = 1;
+  constructor(private service: UserService,
+              private suggestService:SuggestService) {
   }
 
   ngOnInit(): void {
-    this.service.getListPost(this.id).subscribe(data=>{
+    this.suggestService.getSuggestRequest(2).subscribe(suggest=>{
+      this.suggestList = suggest;
+    })
+    this.service.getListPost(this.myId).subscribe(data=>{
       this.listShow = data;
       console.log(this.listShow)
     })
   }
 
+  add(myId: any, idUser: number) {
+    alert(myId);
+    alert(idUser);
+    this.suggestService.addRequest(myId, idUser)
+
+  }
 }
