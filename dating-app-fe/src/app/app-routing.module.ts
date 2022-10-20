5cf7f22eb49a5bc8afa38b1c6c7d0eaf97f5b98d
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-10-20 15:03:42 +0700

    up

--- dating-app-fe/src/app/app-routing.module.ts
+++ dating-app-fe/src/app/app-routing.module.ts
@@ -1,21 +1,10 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
-import {LoginComponent} from "./account/component/login/login.component";
-import {TestComponent} from "./account/component/test/test.component";
-import {AuthGuardService} from "./service/authentication/auth-guard.service";
-import {TestAdminComponent} from "./account/component/test-admin/test-admin.component";
 
-
-const routes: Routes = [
-  {
-    path: "login", component: LoginComponent
-  },
-  {
-    path: "test", component: TestComponent, canActivate: [AuthGuardService]
-  },
-  {
-    path: "test-admin", component: TestAdminComponent, canActivate: [AuthGuardService]
-  }
+const routes: Routes = [{
+  path:'account',
+  loadChildren :()=>import('../app/account/account.module').then(module=>module.AccountModule)
+}
 ];
 
 @NgModule({
